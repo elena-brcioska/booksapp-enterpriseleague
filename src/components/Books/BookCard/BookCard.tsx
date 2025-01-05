f90a75f94ee49329b8d388a253523f864ea066a8
elena-brcioska <elenabrcioska@gmail.com>
2025-01-05 16:44:47 +0100

    fix(BookApp): minor issue fix

--- src/components/Books/BookCard/BookCard.tsx
+++ src/components/Books/BookCard/BookCard.tsx
@@ -4,9 +4,9 @@ import styles from './styles/BookCard.module.css';
 
 const BookCard: FC<IBookCard> = ({ book, searchQuery }) => {
   const highlightText = (text: string, query: string): JSX.Element[] => {
-    if (!query) return [<span key="0">{text}</span>]; // If no query, return the text as is
+    if (!query) return [<span key="0">{text}</span>];
 
-    const parts = text.split(new RegExp(`(${query})`, 'gi')); // Split by query, case-insensitive
+    const parts = text.split(new RegExp(`(${query})`, 'gi'));
     return parts.map((part, index) => {
       if (part.toLowerCase() === query.toLowerCase()) {
         return (
@@ -22,13 +22,13 @@ const BookCard: FC<IBookCard> = ({ book, searchQuery }) => {
   return (
     <div className={styles["book-card"]}>
       <h3 className={styles["book-card__title"]}>
-        {highlightText(book.title, searchQuery)} {/* Highlight title */}
+        {highlightText(book.title, searchQuery)}
       </h3>
       <p className={styles["book-card__author"]}>
-        {highlightText(book.author, searchQuery)} {/* Highlight author */}
+        {highlightText(book.author, searchQuery)}
       </p>
       <p className={styles["book-card__genre"]}>
-        {highlightText(book.genre, searchQuery)} {/* Highlight genre */}
+        {highlightText(book.genre, searchQuery)}
       </p>
     </div>
   );
